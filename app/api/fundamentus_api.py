3280b679b311440b32136f3b60fee99f8ea0e4bf
Tais Prestes <108246691+taisprestes01@users.noreply.github.com>
2024-11-17 18:25:48 -0300

    feat: Add `get_papel` function for detailed stock data retrieval

--- app/api/fundamentus_api.py
+++ app/api/fundamentus_api.py
@@ -1,4 +1,5 @@
 import fundamentus as FundamentusAPI
+import logging
 
 def get_filtered_result(dy_min, dy_max, pvp_min, pvp_max, liquidity_min, price_min):
     """
@@ -33,3 +34,39 @@ def get_filtered_result(dy_min, dy_max, pvp_min, pvp_max, liquidity_min, price_m
         raise ValueError("No stocks match the given criteria.")
 
     return filtered_df
+
+def get_papel(param):
+    """
+    Get detailed data from Fundamentus based on a given stock symbol or a list of symbols.
+
+    URL:
+        http://fundamentus.com.br/detalhes.php?papel=WEGE3
+
+    Input:
+        - param (str or list): The stock symbol as a string or a list of stock symbols.
+
+    Returns:
+        pandas.DataFrame: DataFrame containing detailed information about the specified stock(s).
+
+    Raises:
+        ValueError: If there is an issue with the input type or data retrieval.
+
+    Use example: get_papel('WEGE3')
+    """
+    _type = str(type(param))
+    logging.debug('[param] is of type [{}]'.format(_type))
+
+    try:
+        if _type == "<class 'list'>":
+            logging.info('detalhes: call: get..._list()')
+            return FundamentusAPI.get_detalhes_list(param)
+        else:
+            logging.info('detalhes: call: get..._papel()')
+            return FundamentusAPI.get_detalhes_papel(param)
+
+    except Exception as e:
+        raise ValueError(f"Error fetching or processing the data for the given symbol(s): {str(e)}")
+    
+
+    
+
